7a76b329c5b015fbe791f90826db6ac9090d1fdc
Aleksandr Kazak-Kazakevich <sanbz2007@gmail.com>
2022-07-04 18:39:51 +0300

    Added more API keys

--- SearchFoundView.swift
+++ SearchFoundView.swift
@@ -8,13 +8,48 @@
 import UIKit
 
 class SearchFoundView: UIView {
+    
+    var text: UILabel!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setProperties()
+        
+        createSubviews()
     }
-    */
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setProperties()
+        
+        createSubviews()
+    }
+    
+    func setProperties() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 2
+        layer.cornerCurve = .continuous
+        layer.cornerRadius = 10
+        backgroundColor = .systemGreen
+    }
+    
+    func createSubviews() {
+        text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.text = "FOUND PLACEHOLDER"
+        text.font = .preferredFont(forTextStyle: .largeTitle)
+        text.numberOfLines = 0
+        text.textAlignment = .center
+        addSubview(text)
+        
+        text.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        text.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        text.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        text.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        
+    }
+    
 }
