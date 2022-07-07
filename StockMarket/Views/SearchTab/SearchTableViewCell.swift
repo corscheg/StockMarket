f60290f9c69b623ceebf6e5822fa7e10de921fe8
Aleksandr Kazak-Kazakevich <sanbz2007@gmail.com>
2022-07-07 14:36:58 +0300

    Improved SearchTableViewCell appearance

--- StockMarket/Views/SearchTab/SearchTableViewCell.swift
+++ StockMarket/Views/SearchTab/SearchTableViewCell.swift
@@ -11,27 +11,35 @@ class SearchTableViewCell: UITableViewCell {
 
     var nameLabel: UILabel!
     var tickerLabel: UILabel!
+    var hStack: UIStackView!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        hStack = UIStackView()
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        hStack.axis = .horizontal
+        hStack.alignment = .center
+        hStack.distribution = .equalSpacing
+        addSubview(hStack)
+        
+        hStack.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        hStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15).isActive = true
+        hStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15).isActive = true
+        hStack.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
         
         nameLabel = UILabel()
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.numberOfLines = 1
         nameLabel.font = .preferredFont(forTextStyle: .title1)
-        addSubview(nameLabel)
-        
-        nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
-        nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15).isActive = true
+        hStack.addArrangedSubview(nameLabel)
         
         tickerLabel = UILabel()
         tickerLabel.translatesAutoresizingMaskIntoConstraints = false
         tickerLabel.numberOfLines = 1
         tickerLabel.font = .preferredFont(forTextStyle: .title3)
-        addSubview(tickerLabel)
+        hStack.addArrangedSubview(tickerLabel)
         
-        tickerLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
-        tickerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15).isActive = true
     }
     
     required init?(coder: NSCoder) {
