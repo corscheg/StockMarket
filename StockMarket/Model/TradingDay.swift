47a0a909a353be3a7322598b8fe9ece9cfba163a
Aleksandr Kazak-Kazakevich <sanbz2007@gmail.com>
2022-07-20 11:25:54 +0300

    Added TradingDay struct

---
+++ StockMarket/Model/TradingDay.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  StockMarket
+//
+//  Created by Александр Казак-Казакевич on 20.07.2022.
+//
+
+import Foundation
+
+struct TradingDay: Codable {
+    enum CodingKeys: String, CodingKey {
+        case current = "c"
+        case delta = "d"
+        case deltaPercent = "dp"
+        case high = "h"
+        case low = "l"
+        case open = "o"
+        case previousClose = "pc"
+    }
+    
+    let current: Double
+    let delta: Double
+    let deltaPercent: Double
+    let high: Double
+    let low: Double
+    let open: Double
+    let previousClose: Double
+}
