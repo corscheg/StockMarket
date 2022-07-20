50eb45493ec8cab2b6dd1bb95fe76f60931d9c19
Aleksandr Kazak-Kazakevich <sanbz2007@gmail.com>
2022-07-20 14:49:00 +0300

    Added custom init(from:) to News struct

--- StockMarket/Model/News.swift
+++ StockMarket/Model/News.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct News: Codable, Hashable {
+struct News: Decodable, Hashable {
     enum CodingKeys: String, CodingKey {
         case category
         case dateTime = "datetime"
@@ -28,4 +28,23 @@ struct News: Codable, Hashable {
     let source: String
     let summary: String
     var url: URL?
+    
+    /// Initializer for JSON Decoding.
+    /// - Precondition: `JSONDecoder().dateDecodingStrategy = .secondsSince1970`
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.category = try container.decode(String.self, forKey: .category)
+        self.dateTime = try container.decode(Date.self, forKey: .dateTime)
+        self.title = try container.decode(String.self, forKey: .title)
+        
+        let imageStringURL = try container.decode(String.self, forKey: .imageURL)
+        self.imageURL = URL(string: imageStringURL)
+        
+        self.related = try container.decode(String.self, forKey: .related)
+        self.source = try container.decode(String.self, forKey: .source)
+        self.summary = try container.decode(String.self, forKey: .summary)
+        
+        let stringURL = try container.decode(String.self, forKey: .url)
+        self.url = URL(string: stringURL)
+    }
 }
