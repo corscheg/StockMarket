fc8e37c0432b908860886dccd0bfae116c5f5e03
Aleksandr Kazak-Kazakevich <sanbz2007@gmail.com>
2022-07-21 15:57:28 +0300

    Documented Favorites tab section

--- StockMarket/Views/FavoritesTab/FavoritesPresenter.swift
+++ StockMarket/Views/FavoritesTab/FavoritesPresenter.swift
@@ -7,19 +7,27 @@
 
 import Foundation
 
+/// A class that provides data and methods for Favorites tab.
 class FavoritesPresenter: CompaniesListPresenter {
+    
     private(set) var companies: [Company] = []
+    
+    /// An array of all favorite companies.
     private var unfilteredCompanies: [Company] = []
     weak var view: SearchViewController?
     
     private(set) var viewTitle = "Favorites"
     
+    /// Initializer refreshes data if possible, fills `companies` and updates view.
     init() {
         Task {
+            
+            // Refresh data and set properties
             await FavoritesManager.shared.refreshData()
             unfilteredCompanies = FavoritesManager.shared.favorites
             companies = unfilteredCompanies
             
+            // Update view on the main thread
             DispatchQueue.main.async { [weak self] in
                 self?.updateView()
             }
@@ -28,6 +36,8 @@ class FavoritesPresenter: CompaniesListPresenter {
 }
 
 extension FavoritesPresenter {
+    
+    /// Update the view.
     private func updateView() {
         view?.updateUI()
     }
