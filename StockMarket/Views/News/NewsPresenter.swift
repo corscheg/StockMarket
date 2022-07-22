660baa1e72130d67b2a644954b1bd9ba2bfdfc00
Aleksandr Kazak-Kazakevich <sanbz2007@gmail.com>
2022-07-22 17:46:20 +0300

    Added News view and presenter files

---
+++ StockMarket/Views/News/NewsPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  NewsPresenter.swift
+//  StockMarket
+//
+//  Created by Александр Казак-Казакевич on 22.07.2022.
+//
+
+import Foundation
+
+/// A class for managing news data.
+class NewsPresenter {
+    
+}
---
+++ StockMarket/Views/News/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  StockMarket
+//
+//  Created by Александр Казак-Казакевич on 22.07.2022.
+//
+
+import UIKit
+
+/// A class for displaying the latest news.
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
